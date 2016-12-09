8e56ac61010593ae3f83f86b4339051e946e0d05
MrJaqbq <bentkowski.jakub@gmail.com>
2016-12-09 16:48:49 +0100

    Added BasePawn class

---
+++ Base/Gameplay/BasePawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+    public abstract class BasePawn<T> : Controllable<T>
+    {
+        public virtual void OnLevelLoaded()
+        {
+
+        }
+
+        public virtual void OnLevelCleanUp()
+        {
+
+        }
+
+        public override bool ProcessInput(T processedInput)
+        {
+            return false;
+        }
+
+        public virtual void OnUpdate()
+        {
+
+        }
+    }
+}
