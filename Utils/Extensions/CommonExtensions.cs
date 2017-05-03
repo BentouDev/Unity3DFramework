f8161ad7705aa8054702d7e18e35c48a2175b577
MrJaqbq <bentkowski.jakub@gmail.com>
2017-05-04 00:34:49 +0200

    New extension methods

---
+++ Utils/Extensions/CommonExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CommonExtensions
+{
+    public static T Next<T>(this T src) where T : struct, System.IConvertible
+    {
+        if (!typeof(T).IsEnum)
+            throw new System.ArgumentException (
+                string.Format("Argumnent {0} is not an Enum", 
+                typeof(T).FullName)
+            );
+
+        T[] Arr = (T[])System.Enum.GetValues(src.GetType());
+        int j = System.Array.IndexOf<T>(Arr, src) + 1;
+        return (Arr.Length == j) ? Arr[0] : Arr[j];
+    }
+}
--- Utils/Extensions/MonoGameExtensions.cs
+++ Utils/Extensions/MonoGameExtensions.cs
@@ -1,20 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-static public class MonoBehaviourExtenions {
-	/// <summary>
-	/// Gets or add a component. Usage example:
-	/// BoxCollider boxCollider = transform.GetOrAddComponent<BoxCollider>();
-	/// </summary>
-	static public T GetOrAddComponent<T> (this Component child) where T: Component {
-		T result = child.GetComponent<T>();
-		if (result == null) {
-			result = child.gameObject.AddComponent<T>();
-		}
-		return result;
+public static class MonoBehaviourExtenions
+{
+    public static void TryInit<T>(this Component mono, ref T comp) where T : Component
+    {
+        if (comp == null)
+            comp = mono.GetComponentInChildren<T>();
+    }
+
+    /// <summary>
+    /// Gets or add a component. Usage example:
+    /// BoxCollider boxCollider = transform.GetOrAddComponent<BoxCollider>();
+    /// </summary>
+    public static T GetOrAddComponent<T> (this Component child) where T: Component
+    {
+		T result = child.GetComponent<T>() ?? child.gameObject.AddComponent<T>();
+        return result;
 	}
 
-    static public void SafeDestroy(this MonoBehaviour mono, GameObject obj)
+    public static void SafeDestroy(this Component mono, GameObject obj)
     {
         var pooled = obj.GetComponent<PooledObject>();
         if (pooled)
@@ -23,7 +28,7 @@ static public class MonoBehaviourExtenions {
         }
         else
         {
-            MonoBehaviour.Destroy(obj);
+            Component.Destroy(obj);
         }
     }
 }
