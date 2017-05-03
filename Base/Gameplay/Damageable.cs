a6bcbba30d03bfd4836f4b0aabc2739966de61c9
MrJaqbq <bentkowski.jakub@gmail.com>
2017-05-04 00:34:07 +0200

    Damageable as separate class

---
+++ Base/Gameplay/Damageable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    public bool InitOnStart;
+    public int  MaxHealth;
+    public int  StartingHealth;
+
+    public int  CurrentHealth { get; private set; }
+
+    public bool IsAlive { get { return CurrentHealth > 0; } }
+
+    void Start()
+    {
+        if (!InitOnStart)
+            return;
+
+        Init();
+    }
+
+    public void Init()
+    {
+        CurrentHealth = StartingHealth;
+    }
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        if (!Application.isEditor)
+            return;
+        
+        StartingHealth = Mathf.Clamp(StartingHealth, 0, MaxHealth);
+    }
+#endif
+}
