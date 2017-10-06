6c2f4b6476440514097db357604e51af5b29cf90
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2017-10-06 13:02:30 +0200

    Pawn velocity debug component

---
+++ Base/Gameplay/PawnVelocityDebug.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+    [RequireComponent(typeof(StatePawn))]
+    public class PawnVelocityDebug : MonoBehaviour
+    {
+        public bool Draw;
+        public Color Velocity = Color.red;
+        public Color Direction = Color.yellow;
+        public Color Speed = Color.blue;
+
+        private StatePawn Pawn;
+
+        void Start()
+        {
+            Pawn = GetComponent<StatePawn>();
+        }
+
+        void Update()
+        {
+            if (!Draw)
+                return;
+
+            Debug.DrawRay(Pawn.transform.position, Pawn.Velocity, Velocity);
+            Debug.DrawRay(Pawn.transform.position, Pawn.CurrentDirection.normalized, Direction);
+            Debug.DrawRay(Pawn.transform.position, Pawn.CurrentDirection * Pawn.CurrentSpeed, Speed);
+        }
+    }
+}
