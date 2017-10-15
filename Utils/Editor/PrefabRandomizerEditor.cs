96b0535bd01331942036e60e0550e29cc6d4d694
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2017-10-15 12:53:29 +0200

    Prefab Randomizer

---
+++ Utils/Editor/PrefabRandomizerEditor.cs
@@ -0,0 +1,91 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEditorInternal;
+
+namespace Framework
+{
+    [CustomEditor(typeof(PrefabRandomizer))]
+    public class PrefabRandomizerEditor : UnityEditor.Editor
+    {
+        private bool Folded;
+
+        private ReorderableList List;
+
+        private PrefabRandomizer Target => target as PrefabRandomizer;
+        
+        private const string ToggleLockMenuName = "Prefabs/Lock randomized prefabs";
+        
+        private void OnEnable()
+        {
+            PrefabRandomizer.IsGloballyLocked = EditorPrefs.GetBool(ToggleLockMenuName, false);
+            ToggleGlobalLock(PrefabRandomizer.IsGloballyLocked);
+
+            List = new ReorderableList(Target.Prefabs, typeof(GameObject))
+            {
+                drawElementCallback = (rect, index, active, focused) =>
+                {
+                    rect.y      += 2;
+                    rect.height  = 16;
+                    var result = EditorGUI.ObjectField(rect, Target.Prefabs[index], typeof(GameObject), false) as GameObject;
+                    if (Target.Prefabs[index] != result)
+                    {
+                        Undo.RecordObject(Target, "Changed Prefab");
+                        Target.Prefabs[index] = result;
+                    }
+                },
+                onAddCallback = list =>
+                {
+                    Undo.RecordObject(Target, "Added Prefab");
+                    Target.Prefabs.Add(null);
+                },
+                onRemoveCallback = list =>
+                {
+                    Undo.RecordObject(Target, "Removed Prefab");
+                    Target.Prefabs.RemoveAt(list.index);
+                },
+                drawHeaderCallback = rect =>
+                { }
+            };
+        }
+        
+        [MenuItem(ToggleLockMenuName)]
+        public static void MenuToggleGlobalLock()
+        {
+            ToggleGlobalLock(!PrefabRandomizer.IsGloballyLocked);
+        }
+
+        public static void ToggleGlobalLock(bool enabled)
+        {
+            Menu.SetChecked(ToggleLockMenuName, enabled);
+            EditorPrefs.SetBool(ToggleLockMenuName, enabled);
+
+            PrefabRandomizer.IsGloballyLocked = enabled;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            InspectorUtils.DrawDefaultScriptField(serializedObject);
+
+            EditorGUILayout.Space();
+
+            GUILayout.BeginHorizontal();
+            {
+                if (GUILayout.Button("Randomize"))
+                {
+                    Undo.RegisterCompleteObjectUndo(Target, "Randomize Prefab");
+                    Target.Randomize(true);
+                }
+
+                Target.Locked = GUILayout.Toggle(Target.Locked, Target.Locked ? "Unlock Prefab" : "Lock Prefab");
+            }
+            GUILayout.EndHorizontal();
+
+            Folded = EditorGUILayout.Foldout(Folded, "Prefabs");
+            if (Folded)
+            {
+                List.DoLayoutList();
+                EditorGUILayout.Space();
+            }
+        }
+    }
+}
---
+++ Utils/PrefabRandomizer.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Framework
+{
+    [ExecuteInEditMode]
+    [SelectionBase]
+    public class PrefabRandomizer : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        public List<GameObject> Prefabs;
+
+        [SerializeField]
+        [HideInInspector]
+        public bool Locked = false;
+
+        public static bool IsGloballyLocked;
+
+        private int PickRandom()
+        {
+            return Random.Range(0, Prefabs.Count);
+        }
+
+        public void Randomize(bool force = false)
+        {
+            if (!Prefabs.Any())
+                return;
+
+            if (IsGloballyLocked)
+                return;
+
+            if (Locked && !force)
+                return;
+
+            RemoveChildren();
+
+            Instantiate(Prefabs[PickRandom()], transform);
+        }
+
+        public void RemoveChildren()
+        {
+            var children = gameObject.GetComponentsInChildren<Transform>().Where(t => t != transform);
+            
+            foreach (Transform child in children)
+            {
+                DestroyImmediate(child.gameObject);
+            }
+        }
+        
+        [SerializeField]
+        [HideInInspector]
+        private int localID = 0;
+
+        void Awake()
+        {
+            if (Application.isPlaying)
+                return;
+
+            if (localID == 0)
+            {
+                localID = GetInstanceID();
+                return;
+            }
+
+            if (localID != GetInstanceID() && GetInstanceID() < 0)
+            {
+                localID = GetInstanceID();
+                Randomize();
+            }
+        }
+#endif
+    }
+}
