b47f39689dd6fc9e01cc54072c6b0ed857de83f9
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2017-10-18 18:14:11 +0200

    Extension for list to get sublist from/to specified index

--- Utils/Extensions/CommonExtensions.cs
+++ Utils/Extensions/CommonExtensions.cs
@@ -16,4 +16,18 @@ public static class CommonExtensions
         int j = System.Array.IndexOf<T>(Arr, src) + 1;
         return (Arr.Length == j) ? Arr[0] : Arr[j];
     }
+
+    public static List<T> GetFrom<T>(this List<T> list, int index)
+    {
+        index = Mathf.Clamp(index, 0, list.Count - 1);
+        if (index + 1 > list.Count - 1)
+            return new List<T>();
+
+        return list.GetRange(index + 1, list.Count - 1 - index);
+    }
+
+    public static List<T> GetTo<T>(this List<T> list, int index)
+    {
+        return list.GetRange(0, Mathf.Clamp(index, 0, list.Count - 1));
+    }
 }
