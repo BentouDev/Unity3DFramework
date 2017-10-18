baf0ab12b8dc5b766e8d5f361eeae8bfba20606f
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2017-10-18 18:16:40 +0200

    Damageable has delay and invincibility support

--- Base/Gameplay/Damageable.cs
+++ Base/Gameplay/Damageable.cs
@@ -3,16 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class Damageable : MonoBehaviour
 {
     public bool InitOnStart;
+    
+    public bool Invincible;
     public int  MaxHealth;
     public int  StartingHealth;
+    public float Delay = 1;
+
+    private float LastHurt;
 
     public int  CurrentHealth { get; private set; }
 
     public bool IsAlive { get { return CurrentHealth > 0; } }
-
+    
     void Start()
     {
         if (!InitOnStart)
@@ -26,6 +35,25 @@ public class Damageable : MonoBehaviour
         CurrentHealth = StartingHealth;
     }
 
+    public void Heal(int amount)
+    {
+        CurrentHealth += amount;
+        CurrentHealth  = Mathf.Clamp(CurrentHealth, 0, MaxHealth);
+    }
+
+    public void Hurt(int amount)
+    {
+        if (Invincible)
+            return;
+
+        if (Time.time - LastHurt < Delay)
+            return;
+
+        LastHurt = Time.time;
+        CurrentHealth -= amount;
+        CurrentHealth  = Mathf.Clamp(CurrentHealth, 0, MaxHealth);
+    }
+
 #if UNITY_EDITOR
     void OnValidate()
     {
@@ -33,6 +61,11 @@ public class Damageable : MonoBehaviour
             return;
         
         StartingHealth = Mathf.Clamp(StartingHealth, 0, MaxHealth);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Handles.Label(transform.position + Vector3.up * 1, "HP : " + CurrentHealth + "/" + MaxHealth);
     }
 #endif
 }
