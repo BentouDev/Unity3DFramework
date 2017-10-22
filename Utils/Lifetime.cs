3777bdcfe3fc7277a9d9d14a7159ac940ef690b3
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2017-10-22 02:24:37 +0200

    Lifetime script

---
+++ Utils/Lifetime.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class Lifetime : MonoBehaviour
+{
+    public bool BeginOnStart;
+    public bool DestroyOnEnd;
+    public float Duration;
+
+    public UnityEvent OnEnd;
+
+    void Start()
+    {
+        if (!BeginOnStart)
+            return;
+
+        Begin();
+    }
+
+    public void Begin()
+    {
+        StartCoroutine(CoLifetime());
+    }
+
+    IEnumerator CoLifetime()
+    {
+        yield return new WaitForSeconds(Duration);
+
+        OnEnd.Invoke();
+
+        if (DestroyOnEnd)
+        {
+            yield return new WaitForFixedUpdate();
+            DestroyObject(gameObject);
+        }
+    }
+}
