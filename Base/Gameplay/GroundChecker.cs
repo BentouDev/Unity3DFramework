efb3e08950bce728d427cee5d9fb66954b3865fd
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2017-11-20 23:55:55 +0100

    Dedicated ground checker component

--- Base/Gameplay/GroundChecker.cs
+++ Base/Gameplay/GroundChecker.cs
@@ -1,7 +1,16 @@
-﻿namespace Framework
+﻿using UnityEngine;
+
+namespace Framework
 {
-    public class GroundChecker
+    public abstract class GroundChecker : MonoBehaviour
     {
+        public bool IsGrouneded { get; private set; }
         
+        public void CheckGround()
+        {
+            IsGrouneded = OnCheckGround();
+        }
+
+        protected abstract bool OnCheckGround();
     }
 }
--- Base/Gameplay/SimpleGroundChecker.cs
+++ Base/Gameplay/SimpleGroundChecker.cs
@@ -1,7 +1,48 @@
-﻿namespace Framework
+﻿using UnityEngine;
+
+namespace Framework
 {
-    public class SimpleGroundChecker
+    public class SimpleGroundChecker : GroundChecker
     {
-        
+        [Header("Ground Checking")]
+        public bool DrawHit;
+        public LayerMask RaycastMask;
+        public Vector3 RaycastOrigin;
+        public float RaycastRadius = 1;
+        public float RaycastLength = 1;
+        protected RaycastHit LastGroundHit;
+
+        Vector3[] GetRaycastOffsets()
+        {
+            return new []
+            {
+                transform.rotation * (Vector3.forward * RaycastRadius),
+                transform.rotation * (Vector3.back    * RaycastRadius),
+                transform.rotation * (Vector3.right   * RaycastRadius),
+                transform.rotation * (Vector3.left    * RaycastRadius)
+            };
+        }
+
+        protected override bool OnCheckGround()
+        {
+            var orign = transform.TransformPoint(RaycastOrigin);
+
+            bool _isGrounded = Physics.Raycast(orign, Vector3.down, out LastGroundHit, RaycastLength, RaycastMask);
+            if (_isGrounded && DrawHit)
+            {
+                Debug.DrawLine(LastGroundHit.point, LastGroundHit.point + LastGroundHit.normal, Color.cyan, 5.0f);
+            }
+            else
+            {
+                foreach (Vector3 offset in GetRaycastOffsets())
+                {
+                    _isGrounded |= Physics.Raycast(orign + offset, Vector3.down, out LastGroundHit, RaycastLength, RaycastMask);
+                    if (_isGrounded)
+                        break;
+                }
+            }
+
+            return _isGrounded;
+        }
     }
 }
